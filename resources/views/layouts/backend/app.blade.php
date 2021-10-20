38b4a0074410824c32bd97b0bf24ec73b5566a27
sm-jahangir <samraatjahangir@gmail.com>
2021-10-20 23:34:20 +0600

    Template Mastering Complete

---
+++ resources/views/layouts/backend/app.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    
+    <link href="{{ asset('backend/main.css') }}" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app" class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
+        @include('layouts.backend.partials.header')
+        <div class="app-main">
+            @include('layouts.backend.partials.sidebar')
+            <div class="app-main__outer">
+                <div class="app-main__inner">
+                    @yield('content')
+                </div>
+                @include('layouts.backend.partials.footer')
+            </div>
+            <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+        </div>
+    </div>
+    <script type="text/javascript" src="{{ asset('backend') }}/assets/scripts/main.js"></script>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
