55ca807c1c1c3253960cbb7e87ffcee9c23e74eb
sm-jahangir <samraatjahangir@gmail.com>
2021-10-24 10:30:41 +0600

    Module Permission and Role

---
+++ database/migrations/2021_10_20_174444_create_permissions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('module_id')->constrained('modules')->onDelete('cascade');
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->timestamps();
+            //ager version er code
+            // $table->unsignedBigInteger('module_id');
+            // $table->foreign('module_id')
+            //     ->references('id')
+            //     ->on('modules')
+            //     ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permissions');
+    }
+}
