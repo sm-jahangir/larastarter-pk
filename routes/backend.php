e37e0b1c46809493ac948c0b1cc87841e3f7d2c4
sm-jahangir <samraatjahangir@gmail.com>
2021-10-26 00:37:04 +0600

    Controller Artisan command with button complete

--- routes/backend.php
+++ routes/backend.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\RoleController;
 use App\Http\Controllers\Backend\UserController;
+use App\Http\Controllers\Backend\BackupController;
 use App\Http\Controllers\Backend\DashboardController;
 
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 Route::resource('roles', RoleController::class);
 Route::resource('users', UserController::class);
+Route::resource('backups', BackupController::class)->only(['index', 'store', 'destroy']);
