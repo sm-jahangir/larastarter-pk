0f10d564b70d33fe4bc45eca6bad7abba8f3b1b6
sm-jahangir <samraatjahangir@gmail.com>
2021-10-28 19:27:05 +0600

    General Settings Site Title Update or create  imliam / laravel-env-set-command package v.1.0 use and Artisan command set in controller

--- app/Http/Controllers/Backend/SettingController.php
+++ app/Http/Controllers/Backend/SettingController.php
@@ -15,6 +15,17 @@ class SettingController extends Controller
     }
     public function update(Request $request)
     {
-        return "Hello world";
+        $this->validate($request, [
+            'site_title'    =>  'required|max:255|min:2|string',
+            'site_description'    =>  'nullable|max:255|min:2|string',
+            'site_address'    =>  'nullable|max:255|min:2|string',
+        ]);
+        Setting::updateOrCreate(['name' => 'site_title'], ['value' => $request->get('site_title')]);
+        // Update .env file
+        Artisan::call("env:set APP_NAME='". $request->site_title ."'");
+        notify()->success('Settings Successfully Updated.','Success');
+        Setting::updateOrCreate(['name' => 'site_description'], ['value' => $request->get('site_description')]);
+        Setting::updateOrCreate(['name' => 'site_address'], ['value' => $request->get('site_address')]);
+        return back();
     }
 }
