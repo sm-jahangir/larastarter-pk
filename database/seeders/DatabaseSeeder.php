aa6e852c7ed1c1069e2288ae5733a84ad31eda0a
sm-jahangir <samraatjahangir@gmail.com>
2021-10-28 14:24:52 +0600

    Basic Settings Model m and S Create and Seeding COmplete

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(PageSeeder::class);
         $this->call(MenuSeeder::class);
+        $this->call(SettingSeeder::class);
     }
 }
